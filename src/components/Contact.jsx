f96d36f2bf57cd27643cc9da61b0d1b30906dcd0
aszab1 <adry_szabo@yahoo.co.uk>
2024-02-14 15:12:49 +0000

    added about me page including description, skills and about me

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,11 @@
 import Form from "./Form"
-import contactme1 from '../assets/images/contactme1.jpg'
+import contact from '../assets/images/contact.png'
 
 
 export default function Contact (){
   return (
     <>
-  <img className="contact-img" src={contactme1} alt='image of a phone with a Contact Me text'/>
+  <img className="contact-img" src={contact} alt='image of a phone with a Contact Me text'/>
       <h4 className="contact-h4">Get in Touch</h4>
   <div className="contact-container">
     <div className="contact-text">
